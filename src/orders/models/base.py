0f914ed9a52b10a27b903a387f23abdfe4c13515
Gabriel C. Ullmann <ullmanng162@gmail.com>
2025-12-30 12:59:54 -0500

    update deprecated import

--- src/orders/models/base.py
+++ src/orders/models/base.py
@@ -4,7 +4,7 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
 # Create the shared declarative base that all models will inherit from
 Base = declarative_base()
